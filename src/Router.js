035f354e26a591aebe97ff5ea26e76c7aa7f6921
dulcefabiola <62074962+DulceFabiola@users.noreply.github.com>
2021-12-18 11:33:47 -0600

    user-routes

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+import Profile from "./components/User/Profile";
 const Router = () => {
   return (
     <>
@@ -9,6 +12,10 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            {/* Rutas de Usuario */}
+            <Route path="signup" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
