66e470e613a263d889f195a9aedf7900d543ce12
dulcefabiola <62074962+DulceFabiola@users.noreply.github.com>
2021-12-18 12:36:39 -0600

    courses-users views

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,10 @@ import Layout from "./components/Layout";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 import Profile from "./components/User/Profile";
+import Courses from "./components/Courses";
+import CreateCourses from "./components/Courses/CreateCourse";
+import SingleCourse from "./components/Courses/SingleCourse";
+import EditCourse from "./components/Courses/EditCourse";
 const Router = () => {
   return (
     <>
@@ -13,10 +17,16 @@ const Router = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             {/* Rutas de Usuario*/}
-            <Route path="signup" element={<Register />} />
-            <Route path="login" element={<Login />} />
             <Route path="profile" element={<Profile />} />
+            {/* Rutas de curso*/}
+            <Route path="courses" element={<Courses />} />
+            <Route path="courses/create" element={<CreateCourses />} />
+            <Route path="courses/:id" element={<SingleCourse />} />
+            <Route path="courses/:id/edit" element={<EditCourse />} />
           </Route>
+          {/* Rutas de Usuario*/}
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
