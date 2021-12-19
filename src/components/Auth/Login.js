8fb882c4b98b0c313c28f0fafa41817ba44b7469
dulcefabiola <62074962+DulceFabiola@users.noreply.github.com>
2021-12-19 15:11:03 -0600

    fix-login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -101,14 +101,13 @@ const Login = () => {
                       </Link>
                     </div>
                   </div>
-                  <Link to="/profile">
-                    <button
-                      type="submit"
-                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Inicia sesión
-                    </button>
-                  </Link>
+
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Inicia sesión
+                  </button>
                 </form>
               </div>
             </div>
