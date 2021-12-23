a18d2a9fce2e7398855895f700155d2caeb67f8d
dulcefabiola <62074962+DulceFabiola@users.noreply.github.com>
2021-12-23 00:06:45 -0600

    limpieza de codigo

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -38,87 +38,86 @@ const Courses = () => {
   };
   return (
     <>
-      <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-        <div class="absolute inset-0">
-          <div class="bg-white h-1/3 sm:h-2/3"></div>
+      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="absolute inset-0">
+          <div className="bg-white h-1/3 sm:h-2/3"></div>
         </div>
-        <div class="relative max-w-7xl mx-auto">
-          <div class="text-center">
-            <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+        <div className="relative max-w-7xl mx-auto">
+          <div className="text-center">
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
               Elige tu próximo grupo
             </h2>
-            <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+            <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
               Puedes tomar grupos de tu nivel actual o de niveles anteriores
               para reforzar
             </p>
           </div>
-          <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-            {coursesLocal.map((element) => {
+          <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+            {coursesLocal.map((element, i) => {
               return (
                 <>
                   <Link to={`/courses/${element._id}`} key={element._id}>
-                    <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                      <div class="flex-shrink-0">
+                    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                      <div className="flex-shrink-0">
                         <img
-                          class="h-48 w-full object-cover"
+                          className="h-48 w-full object-cover"
                           src={element.image}
                           alt=""
                         />
                       </div>
-                      <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-                        <div class="flex-1">
-                          <p class="text-xl font-semibold text-gray-900">
+                      <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                        <div className="flex-1">
+                          <p className="text-xl font-semibold text-gray-900">
                             {element.language}
                           </p>
-                          <p class="text-sm font-medium text-indigo-600">
+                          <p className="text-sm font-medium text-indigo-600">
                             {element.level}
                           </p>
-                          <p class="mt-3 text-base text-gray-500">
+                          <p className="mt-3 text-base text-gray-500">
                             <span className="font-medium text-gray-600">
                               Fecha de incripción:
                             </span>
                             {element.inscriptionDate}
                           </p>
-                          <p class="mt-3 text-base text-gray-500">
+                          <p className="mt-3 text-base text-gray-500">
                             <span className="font-medium text-gray-600">
                               Fecha de inicio:
                             </span>
                             {element.startDate}
                           </p>
-                          <p class="mt-3 text-base text-gray-500">
+                          <p className="mt-3 text-base text-gray-500">
                             <span className="font-medium text-gray-600">
                               Días:
                             </span>
                             {element.days}
                           </p>
-                          <p class="mt-3 text-base text-gray-500">
+                          <p className="mt-3 text-base text-gray-500">
                             <span className="font-medium text-gray-600">
                               Horario:
                             </span>
                             {element.schedule}
                           </p>
                         </div>
-                        {teachers.map((teacher) => {
+                        {teachers.map((teacher, i) => {
                           if (teacher._id === element.owner) {
                             return (
-                              <Link
-                                key={teacher._id}
-                                to={`/teachers/${teacher._id}`}
-                              >
-                                <div class="mt-6 flex items-center">
-                                  <div class="flex-shrink-0">
-                                    <span class="sr-only">{teacher.name}</span>
+                              <Link key={i} to={`/teachers/${teacher._id}`}>
+                                <div className="mt-6 flex items-center">
+                                  <div className="flex-shrink-0">
+                                    <span className="sr-only">
+                                      {teacher.name}
+                                    </span>
                                     <img
-                                      class="h-10 w-10 rounded-full"
+                                      className="h-10 w-10 rounded-full"
                                       src={teacher.image}
                                       alt=""
                                     />
                                   </div>
-                                  <div class="ml-3">
-                                    <p class="text-sm font-medium text-gray-900">
+                                  <div className="ml-3">
+                                    <p className="text-sm font-medium text-gray-900">
                                       Teacher: {teacher.name}
                                     </p>
-                                    <div class="flex space-x-1 text-sm text-gray-500">
+                                    <div className="flex space-x-1 text-sm text-gray-500">
                                       <time dateTime="2020-03-16">
                                         Más detalles
                                       </time>
